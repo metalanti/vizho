087df9069e3c236e634dda7a2b721e2767ee3bfd
anti <antal.laszlo@ferlingwebline.hu>
2017-06-06 10:02:38 +0200

    aktualis

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,10 @@ $(function() {
 	});
 
 	$.getJSON('/data', function(resp) {
+		if (resp && resp.length > 0) {
+			var utolso = resp[resp.length - 1];
+			$('#utolso').html('<small>' + moment(utolso.date).format('YYYY-MM-DD HH:mm') + '</small> ' + utolso.temp + ' °C');
+		}
 		data = resp.map(function(r) {
 			return [r.date, r.temp];
 		});
